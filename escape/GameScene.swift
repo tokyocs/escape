6beeb586dda919fe4e036e07f303d72d844b8a9f
tcs16172 <tcs16172@tcs-mac-1803.local>
2018-10-04 11:02:02 +0900

    主人公追加

--- escape/GameScene.swift
+++ escape/GameScene.swift
@@ -10,6 +10,12 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var earth: SKSpriteNode!
+    
+    
+    var left_button: SKSpriteNode!
+    
+    var right_button: SKSpriteNode!
     
     var newmap: SKSpriteNode!
     var hero: SKSpriteNode!
@@ -18,6 +24,22 @@ class GameScene: SKScene {
     private var label : SKLabelNode?
     
     override func didMove(to view: SKView) {
+        // left_buttonを画像登録して表示する
+        self.left_button = SKSpriteNode(imageNamed: "left_button")
+        self.left_button.position = CGPoint(x:1000 , y: -2000)
+        self.left_button.xScale = 0.3
+        self.left_button.yScale = 0.3
+        self.left_button.zPosition = 0
+        addChild(self.left_button)
+        
+        // right_buttonを画像登録して表示する
+        self.right_button = SKSpriteNode(imageNamed: "right_button")
+        self.right_button.position = CGPoint(x: 1000 , y: 2000)
+        self.right_button.xScale = 0.3
+        self.right_button.yScale = 0.3
+        self.right_button.zPosition = 0
+        addChild(self.right_button)
+
         self.newmap = SKSpriteNode(imageNamed: "newmap")
         self.newmap.xScale = 20
         self.newmap.yScale = 20
@@ -26,7 +48,7 @@ class GameScene: SKScene {
         addChild(self.newmap)
         self.hero = SKSpriteNode(imageNamed: "hero")
         self.hero.scale(to: CGSize(width: frame.width / 5, height: frame.width / 5))
-        self.hero.position = CGPoint(x: 0, y: self.newmap.frame.maxY + 50)
+        self.hero.position = CGPoint(x: 0, y:0)
         addChild(self.hero)
 
     }
@@ -52,6 +74,26 @@ class GameScene: SKScene {
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // タッチされているオブジェクトから、
+        for touch: AnyObject in touches {
+            // タッチした場所を取得する。
+            let location = touch.location(in: self)
+            // タッチされたノードを選択して、ボタンと触れたかを判定する。
+            let touchNode = self.atPoint(location)
+            // 右のボタンが押されたら右に30px動かす。
+            if touchNode == right_button {
+                // 右に動く動きを指定する。
+                let moveToRight = SKAction.moveTo(x: self.hero.position.x + 30, duration: 0.2)
+                // 右に動かす。
+                hero.run(moveToRight)
+                // 左のボタンが押されたら左に30px動かす。
+            }else if touchNode == left_button {
+                // 左に動く動きを指定する。
+                let moveToLeft = SKAction.moveTo(x: self.hero.position.x - 30, duration: 0.2)
+                // 左に動く動きを指定する。
+              hero.run(moveToLeft)
+            }
+        }
         
     }
     
